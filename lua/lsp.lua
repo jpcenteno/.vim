d4f49c9ba67304da45850e41a6ef59cc4d0dd648
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2022-10-14 03:47:58 -0300

    Minor changes

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,9 +4,19 @@ local lspconfig = require('lspconfig')
 
 mason.setup()
 
+-- What value is `mason-lspconfig` providing here?
 mason_lspconfig.setup({
     ensure_installed = { "rust_analyzer" },
     automatic_installation = true,
 })
 
-lspconfig['rust_analyzer'].setup{}
+local on_attach = function(client, bufnr)
+end
+
+local servers = { 'rust_analyzer' }
+
+for _, server in ipairs(servers) do
+    lspconfig[server].setup {
+        on_attach = on_attach
+    }
+end
