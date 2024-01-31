ad1da890e58d52e26d33e3db73637ccaa4515633
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-01-31 15:20:59 -0300

    Obsidian: cross platform URL opener

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -12,4 +12,15 @@ require("obsidian").setup({
       if title == nil then error("Title must be non-nil") end
       return title:gsub(" ", "-"):gsub("_", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
     end,
+
+    follow_url_func = function(url)
+      if vim.fn.has("macunix") == 1 then
+        print("this is macunix")
+        vim.fn.jobstart({"open", url})
+      elseif vim.fn.has("linux") == 1 and vim.fn.executable("xdg-open") == 1 then
+        vim.fn.jobstart({"xdg-open", url})  -- linux
+      else
+        error("Unable to find URL opener for this system. Edit 'follow_url_func' to fix this issue.")
+      end
+    end,
 })
