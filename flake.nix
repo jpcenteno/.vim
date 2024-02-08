b30d3e332af4e05dce1ea876745629a4854a0393
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-02-07 22:45:51 -0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "My personal Neovim configuration.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells = rec {
+        default = pkgs.mkShell {
+          packages = [ pkgs.lua-language-server ];
+        };
+      };
+    }
+  );
+}
