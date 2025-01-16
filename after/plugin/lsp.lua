54d80b1f2460191a01c87d910d1a75e9a668b775
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-01-16 12:39:05 -0300

    Lua LS: Scan nvim's runtimepath

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -85,13 +85,13 @@ lspconfig.lua_ls.setup {
           -- Make the server aware of Neovim runtime files
           workspace = {
             checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME
-              -- "${3rd}/luv/library"
-              -- "${3rd}/busted/library",
-            }
+            -- library = {
+            --   vim.env.VIMRUNTIME
+            --   -- "${3rd}/luv/library"
+            --   -- "${3rd}/busted/library",
+            -- }
             -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-            -- library = vim.api.nvim_get_runtime_file("", true)
+            library = vim.api.nvim_get_runtime_file("", true)
           }
         }
       })
