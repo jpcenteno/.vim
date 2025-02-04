35bdeeb05ccd5f8b7ee403b353aeb98638e6d6e1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-02-04 15:08:13 -0300

    refactor(kasten): Refactor kasten note file picker

--- myplugins/kasten/lua/kasten/telescope.lua
+++ myplugins/kasten/lua/kasten/telescope.lua
@@ -1,10 +1,7 @@
-local M = {}
-
-function M.note_open()
+local function list_notes()
   local handle = io.popen("kasten-cli note list --json", "r")
   if not handle then
-    vim.notify("Failed to execute `kasten-cli note list`", vim.log.levels.ERROR)
-    return
+    error("Failed to execute `kasten-cli note list`")
   end
 
   local result = handle:read("*a")
@@ -12,52 +9,50 @@ function M.note_open()
 
   local success, notes = pcall(vim.json.decode, result)
   if not success then
-    vim.notify("Failed to parse JSON output from `kasten-cli note list`", vim.log.levels.ERROR)
-    return
+    error("Failed to parse JSON output from `kasten-cli note list`")
   end
 
   if #notes == 0 then
-    vim.notify("No notes found", vim.log.levels.WARN)
-    return
+    error("No notes found")
   end
 
-  local pick_items = {}
-  for _, note in ipairs(notes) do
-    table.insert(pick_items, {
-      title = note.title,
-      path = note.absolutePath
-    })
-  end
+  return notes
+end
 
-  require('telescope.pickers').new({}, {
-    prompt_title = 'Note Zettels',
-    finder = require('telescope.finders').new_table({
-      results = pick_items,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry.title,
-          ordinal = entry.title,
-        }
+local note_picker = require('telescope.pickers').new({}, {
+  prompt_title = 'Open note',
+  finder = require('telescope.finders').new_table({
+    results = list_notes(),
+    entry_maker = function(entry)
+      return {
+        value = entry,
+        display = entry.title,
+        ordinal = entry.title,
+      }
+    end
+  }),
+  sorter = require("telescope.config").values.generic_sorter({}),
+  attach_mappings = function(prompt_bufnr, _)
+    local actions = require('telescope.actions')
+    local action_state = require('telescope.actions.state')
+
+    actions.select_default:replace(function()
+      actions.close(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
+      if selection then
+        local filepath = selection.value.absolutePath
+        vim.cmd("edit " .. vim.fn.fnameescape(filepath))
       end
-    }),
-    sorter = require("telescope.config").values.generic_sorter({}),
-    attach_mappings = function(prompt_bufnr, map)
-      local actions = require('telescope.actions')
-      local action_state = require('telescope.actions.state')
+    end)
 
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        if selection then
-          local filepath = selection.value.path
-          vim.cmd("edit " .. vim.fn.fnameescape(filepath))
-        end
-      end)
+    return true
+  end,
+})
 
-      return true
-    end,
-  }):find()
+local M = {}
+
+function M.note_open()
+  note_picker:find()
 end
 
 return M
