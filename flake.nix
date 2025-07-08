ec97a9be8c55cd25670aa5e7c726db03a2573bdb
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-07-08 13:38:39 -0300

    refactor: Remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -3,23 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells = rec {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            lua-language-server
-            nodePackages.vim-language-server
-          ];
-        };
+  outputs = { self, nixpkgs, systems }:
+  let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  in
+  {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          lua-language-server
+          nodePackages.vim-language-server
+        ];
       };
-    }
-  );
+    });
+  };
 }
