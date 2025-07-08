bf9888fbe6e5078bb2c26f453a65f13f434042f8
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-07-08 13:47:15 -0300

    chore: Setup formatting check

--- flake.nix
+++ flake.nix
@@ -3,11 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+        treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, systems }:
+  outputs = { self, nixpkgs, systems , treefmt-nix }:
   let
     eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+          treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
   in
   {
     devShells = eachSystem (pkgs: {
@@ -17,6 +23,10 @@
           nodePackages.vim-language-server
         ];
       };
+    });
+
+    checks = eachSystem (pkgs: {
+      formatting = treefmtEval.${pkgs.system}.config.build.check self;
     });
   };
 }
