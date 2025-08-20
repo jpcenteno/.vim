6c6f20f506bd4deb041bb84e4801148e6021cb00
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-08-20 03:13:36 -0300

    fix format

--- LuaSnip/clojure.lua
+++ LuaSnip/clojure.lua
@@ -3,5 +3,5 @@ local s = ls.snippet
 local t = ls.text_node
 
 ls.add_snippets("clojure", {
-  s("unimplemented", t("(throw (UnsupportedOperationException. \"Unimplemented!\"))"))
+  s("unimplemented", t('(throw (UnsupportedOperationException. "Unimplemented!"))')),
 })
