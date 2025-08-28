3586b39d6c06931171d097c5ea272ffb0ec01ae7
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-08-28 15:07:29 -0300

    Replace telescope pickers with snacks

--- after/plugin/snacks.lua
+++ after/plugin/snacks.lua
@@ -2,7 +2,32 @@ local snacks = require('snacks')
 
 snacks.setup({
   picker = {
-    ui_select = true, -- vim.ui.select({ 'a', 'b', 'c' }, {}, function(_, _) end)
-  }
+    ui_select = true,
+  },
+  notifier = {}
+})
 
+vim.api.nvim_set_keymap('n', '<leader>bb', '', {
+  callback = snacks.picker.buffers,
+  desc = "Find Buffers",
+})
+
+vim.api.nvim_set_keymap('n', '<leader>ff', '', {
+  callback = snacks.picker.files,
+  desc = "Find files",
+})
+
+vim.api.nvim_set_keymap('n', '<leader>fg', '', {
+  callback = snacks.picker.git_files,
+  desc = 'Find Git files',
+})
+
+vim.api.nvim_set_keymap('n', '<leader>fr', '', {
+  callback = snacks.picker.grep,
+  desc = 'Grep',
+})
+
+vim.api.nvim_set_keymap('n', '<leader><leader>', '', {
+  callback = snacks.picker.commands,
+  desc = 'Commands',
 })
