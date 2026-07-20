887c0b7fe7d56c976c764a479b555c2673b9f3ce
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-07-20 19:30:18 -0300

    meta (refactor): Remove unused modules

--- myplugins/kasten/lua/kasten.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_set_keymap(
-  "v",
-  "<localleader>ln",
-  ':lua require("kasten.link").insert_link()<CR>',
-  { noremap = true, silent = true }
-)
--- myplugins/kasten/lua/kasten/utils/config.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.get_kasten_dir = function()
-  return os.getenv("KASTEN_DIR")
-end
-
-return M
