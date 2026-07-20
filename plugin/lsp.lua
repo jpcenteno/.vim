5f92c67dd08de8349ebb00677aaf164e94a9667f
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2026-07-20 12:38:10 -0300

    Fix diagnostic jump direction
    Fixes a bug where I `[d` and `]d` were to the inverse of the direction
    that feels natural to me. This PR inverts it.

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -16,10 +16,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf, desc = "Rename symbol" })
 
     vim.keymap.set("n", "[d", function()
-      vim.diagnostic.jump({ count = 1, float = true })
-    end)
-    vim.keymap.set("n", "]d", function()
       vim.diagnostic.jump({ count = -1, float = true })
+    end)
+
+    vim.keymap.set("n", "]d", function()
+      vim.diagnostic.jump({ count = 1, float = true })
     end)
   end,
 })
